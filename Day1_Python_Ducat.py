b85cf23fa87ac91550aa339cfc589ec5bff39f78
Skp8108 <48813966+Skp8108@users.noreply.github.com>
2021-02-02 01:31:11 +0530

    Add files via upload

---
+++ Day1_Python_Ducat.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[59]:
+
+
+#Operators
+#1 Arithmetic Operators
+# + :addition
+# - :subtraction
+# * :Multiplication
+# / :Division
+# // :Floor Division
+# ** :Exponent
+# % :Modulus
+
+#1 Arithmetic Operators practice
+print("********Arithmetic Operators********")
+print("Addition:- ",2+2)
+print("Subtraction:- ",4-2)
+print("Multiplication:- ",2*2)
+print("Division:- ",4/2)
+print("Floor Divison:- ",4//2)
+print("Exponent:- ",2**2)
+print("Modulus:- ",4%2)
+
+#2 Assignment Operators
+# = Assigns values from right side operands to left side operand
+# += Add AND assign value
+# -= Subtract AND assign value
+# *= Multiply AND assign value
+# /= Divide AND assign value
+# %= Modulus AND assign value
+# **= Exponent AND assign value
+# //= Floor Division assign value
+
+#2 Assignment Operators Practice
+print("\n********Assignment Operators********")
+x=10
+print(x)
+x +=5
+print(x)
+x -=5
+print(x)
+x *=5
+print(x)
+x=10
+x /=5
+print(x)
+x=10
+x %=5
+print(x)
+x=10
+x //=5
+print(x)
+x=10
+x **= 2
+print(x)
+
+#3 Comparision/Relational Operators 
+# ==
+# !=
+# >
+# <
+# >=
+# <=
+
+#3 Comparision operators practice
+print("\n********Comparision Operators********")
+x = 5
+y = 10
+print(x==y)
+print(x!=y)
+print(x<y)
+print(x>y)
+print(x>=y)
+print(x<=y)
+
+#4 Bitwise Operators
+# & Binary AND
+# | Binary OR
+# ^ Binary XOR
+# ~ Binary Ones Complement
+# << Binary Left Shift
+# >> Binary Right Shift
+
+#4 Bitwise Operators practice
+print("\n********Bitwise Operators********")
+a=2
+b=3
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~a)
+print(a <<2)
+print(a >>2)
+
+#5 Logical Operators
+# and 
+# or
+# not
+
+#5 Logical Operators Practice
+print("\n********Logical Operators********")
+a=True
+b=False
+print(a and b)
+print(a or b)
+print(not a)
+
+#6 Membership Operators
+# in
+# not in
+
+#6 Membership Operators practice
+x=10
+y=[10,20,30]
+print("\n********Membership Operators********")
+print( x in y)
+print(x not in y)
+
+#7 Identity Operators
+# is 
+# is not
+
+#7 Identity Operators practice
+print("\n********Identity Operators********")
+a=5
+b=5
+print(a is b)
+print( a is not b)
+
+
+# In[34]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
