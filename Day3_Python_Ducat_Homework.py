a3f39abb2056b2a00d34782c9386c04dbfda875e
Skp8108 <48813966+Skp8108@users.noreply.github.com>
2021-02-02 22:07:27 +0530

    Add files via upload

---
+++ Day3_Python_Ducat_Homework.py
@@ -0,0 +1,18 @@
+i=1
+j=1
+s=5
+for x in range(5):
+    print(' '*s,end='')
+    print(i)
+    j=j*10+1
+    i=j*j
+    s -=1
+i=1
+j=j//100
+s=2
+for x in range(4):
+    print(' '*s,end='')
+    i=j*j
+    print(i)
+    j=j//10
+    s +=1
